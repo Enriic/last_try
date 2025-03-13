bb95ad9f33fa8ae89e488072f37fa014d293d4e5
Enric Cancer <enric.cancer@estudiants.urv.cat>
2025-03-13 13:57:38 +0100

    sd

--- tfg_ctaima_back/settings.py
+++ tfg_ctaima_back/settings.py
@@ -214,8 +214,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'staticfiles/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Si tienes archivos estáticos personalizados
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
